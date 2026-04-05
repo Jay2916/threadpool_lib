d94604acdbc393c06199ddd5c28bb3d0c68fb495
Jay <jay@MacBook.local>
2026-04-05 19:18:26 +0530

    Reworked into distributed task queue with task stealing

--- threadpool.c
+++ threadpool.c
@@ -6,107 +6,153 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <semaphore.h>
+#include <time.h>
 #include "../util/queue.h"
 #include "threadpool.h"
 
-#define MAX 100
+#define MAX_TASKS 100
 
-struct job_t{
-    void (*function)(void*);
-    void *arg;
+struct task{
+        void (*function)(void*);
+        void *arg;
 };
 
-typedef enum STOP_COND{FALSE, IMMEDIATE, GRACEFULL} STOP_COND;
+
 struct threadpool{
-    STOP_COND stop;
-    pthread_cond_t enqueue_cond;
-    pthread_cond_t dequeue_cond;
-    pthread_mutex_t mutex;
-    int nthread;
-    Queue *job_queue;
-    pthread_t *thread;
+        atomic_bool stop;
+        pthread_mutex_t attr_mutex;
+        pthread_cond_t empty_cond;
+        unsigned int total_tasks;
+        unsigned int max_threads;
+        unsigned int next_index;
+        threadinfo *thread_array;
 };
 
-void* worker_routine(void* arg){
-    threadpool *tp = (threadpool*)arg;
-    job_t job;
-    
-    while(true){
-        pthread_mutex_lock(&tp->mutex);
-        while(is_empty(tp->job_queue) && (tp->stop == FALSE) ){
-            pthread_cond_wait(&tp->enqueue_cond, &tp->mutex);
+struct threadinfo{
+        pthread_t id;
+        Queue *task_queue;
+        pthread_mutex_t queue_mutex;
+        threadpool *tp;
+};
+
+void *worker(void* arg){
+        threadinfo *curr = arg;
+        threadpool *tp = curr->tp;
+        while(!atomic_load(&tp->stop)){
+                task t = {NULL, NULL};
+                pthread_mutex_lock(&curr->queue_mutex);
+                if(is_empty(curr->task_queue)){ 
+                        pthread_cond_signal(&tp->empty_cond);
+                        pthread_mutex_unlock(&curr->queue_mutex);
+                        t = threadpool_steal(curr); 
+                }
+                else{
+                        dequeue_front(curr->task_queue, &t);
+                        pthread_mutex_unlock(&curr->queue_mutex);
+                }
+                if(t.function != NULL){
+                        t.function(t.arg);
+                        pthread_mutex_lock(&tp->attr_mutex);
+                        tp->total_tasks--;
+                        pthread_mutex_unlock(&tp->attr_mutex);
+                }      
         }
-        if(tp->stop == IMMEDIATE || (tp->stop == GRACEFULL && is_empty(tp->job_queue))){
-            pthread_mutex_unlock(&tp->mutex);
-            break;
+        return NULL;
+}
+threadpool *threadpool_create(int num_of_threads){
+        threadpool *tp = malloc(sizeof(threadpool));
+        if(tp == NULL){
+                return NULL;
         }
-        dequeue(tp->job_queue, &job);
-        pthread_cond_signal(&tp->dequeue_cond);
-        pthread_mutex_unlock(&tp->mutex);
-        job.function(job.arg);
-    }
-    return NULL;
-    
+        tp->next_index = 0;
+        atomic_init(&tp->stop, false);
+        pthread_mutex_init(&tp->attr_mutex, NULL);
+        tp->max_threads = num_of_threads;
+        tp->total_tasks = 0;
+        pthread_cond_init(&tp->empty_cond, NULL);
+        tp->thread_array = calloc(num_of_threads, sizeof(threadinfo));
+        if(tp->thread_array == NULL){
+                free(tp);
+                return NULL;
+        }        
+        for(int i = 0; i < tp->max_threads; i++){
+                threadinfo *curr = tp->thread_array + i;
+                curr->tp = tp;
+                
+                curr->task_queue = create_queue(MAX_TASKS, sizeof(task));
+                pthread_mutex_init(&curr->queue_mutex, NULL);     
+        }
+        for(int i = 0; i < tp->max_threads ;i++){
+                threadinfo *curr = tp->thread_array + i;
+                pthread_create(&curr->id, NULL, worker, curr);
+                printf("Thread %d created.\n",i);
+        }
+        return tp;
 }
+void threadpool_submit(threadpool *tp, void (*task_function)(void*), void *task_arg){
+        task t = {task_function, task_arg};
+        threadinfo *thread_array = tp->thread_array;
+        int i = tp->next_index;
+        threadinfo *next_thread = thread_array + i;
+        pthread_mutex_lock(&next_thread->queue_mutex);
+        while(is_full(next_thread->task_queue)){
+                pthread_mutex_unlock(&next_thread->queue_mutex);
+                i = (i + 1) % tp->max_threads;
+                pthread_mutex_lock(&next_thread->queue_mutex);
+        }
+        enqueue_rear(next_thread->task_queue, &t);
+        pthread_mutex_lock(&tp->attr_mutex);
+        tp->total_tasks++;
+        pthread_mutex_unlock(&tp->attr_mutex);
+        pthread_mutex_unlock(&next_thread->queue_mutex); 
 
-//mem leak: if one of the allocation call fails others have to be freed 
-//assumed no failed allocations
-threadpool* threadpool_create(int n){ 
-    threadpool* tp = (threadpool*)malloc(sizeof(threadpool));
-    if(tp == NULL){
-        return NULL;
-    }
-    tp->nthread = 0;
-    tp->stop = FALSE;
-    pthread_cond_init(&tp->enqueue_cond, NULL);
-    pthread_cond_init(&tp->dequeue_cond, NULL);
-    tp->job_queue = create_queue(MAX, sizeof(job_t));
-    tp->thread = (pthread_t*)calloc(n, sizeof(pthread_t));
-    pthread_mutex_init(&tp->mutex, NULL);
-
-    if(tp->job_queue == NULL || tp->thread == NULL){
-        return NULL;
-    }
-    for(int i = 0; i < n; i++){
-        tp->nthread++;
-        pthread_create(tp->thread + i, NULL, worker_routine, (void*)tp);
-        printf("Thread %d created.\n", i);
-    }
-    return tp;
-}
-
-//blocking submit => induces backpressure in queue
-//can be made non blocking which returns error on Q_FULL
-//some other ways to manage backpressure: unbounded queue(bad for general purpore threadpools, make user thread execute job when queue full)
-void threadpool_submit(threadpool* tp, void (*function)(void*), void* arg){ 
-    job_t job = {function, arg};
-
-    pthread_mutex_lock(&tp->mutex);
-    while(is_full(tp->job_queue) && tp->stop == FALSE){
-        pthread_cond_wait(&tp->dequeue_cond, &tp->mutex);
-    }
-    enqueue(tp->job_queue, &job);
-    pthread_cond_signal(&tp->enqueue_cond);
-    pthread_mutex_unlock(&tp->mutex);
-    
 
 }
-
-void threadpool_destroy(threadpool* tp, bool immediate_shutdown){ 
-    pthread_mutex_lock(&tp->mutex);
-    tp->stop = (immediate_shutdown)? IMMEDIATE : GRACEFULL;
-    pthread_mutex_unlock(&tp->mutex);
-    pthread_cond_broadcast(&tp->enqueue_cond);
-    pthread_cond_broadcast(&tp->dequeue_cond);
-    for(int i = 0;i<tp->nthread;i++){
-        pthread_join(tp->thread[i], NULL);
-        printf("Thread %d joined.\n",i);
-    }
-    pthread_mutex_destroy(&tp->mutex);
-    pthread_cond_destroy(&tp->enqueue_cond);
-    pthread_cond_destroy(&tp->dequeue_cond);
-    destroy_queue(tp->job_queue);
-    free(tp->thread);
-    free(tp);
+task threadpool_steal(threadinfo *thief_thread){
+        threadinfo *victim_thread;
+        threadpool *tp = thief_thread->tp; 
+        task t = {NULL, NULL};
+        unsigned int seed = time(NULL) ^ (uintptr_t)thief_thread->id;
+        int status;
+        int counter = 0;
+        int repeat = 1; // <= tp->max_threads
+        do
+        {       int idx = rand_r(&seed) % tp->max_threads;
+                victim_thread = tp->thread_array + idx;
+                pthread_mutex_lock(&victim_thread->queue_mutex);
+                status = dequeue_front(victim_thread->task_queue, &t);
+                pthread_mutex_unlock(&victim_thread->queue_mutex);
+                counter++;
+        }while(status != Q_OKAY && counter < repeat);
+        return t;
 }
-
+void threadpool_free(threadpool *tp){
+        threadinfo *temp = tp->thread_array;
+        for(int i = 0; i < tp->max_threads; i++){
+                threadinfo *curr = temp + i;
+                destroy_queue(curr->task_queue);
+                pthread_mutex_destroy(&curr->queue_mutex);
+        }
+         pthread_cond_destroy(&tp->empty_cond);
+        pthread_mutex_destroy(&tp->attr_mutex);
+        free(temp);
+        free(tp);
+}
+void threadpool_wait(threadpool *tp){ //blocks till all the tasks are completed
+        pthread_mutex_lock(&tp->attr_mutex);
+        while(tp->total_tasks > 0){
+                pthread_cond_wait(&tp->empty_cond, &tp->attr_mutex);
+        }
+        pthread_mutex_unlock(&tp->attr_mutex);
+        printf("all tasks completed.\n");
+        
+}
+void threadpool_destroy(threadpool *tp){
+        atomic_store(&tp->stop, true);
+        for(int i = 0;i < tp->max_threads; i++){
+                threadinfo *curr = tp->thread_array + i;
+                pthread_join(curr->id, NULL);
+                printf("Thread %d joined.\n",i);
+        }
+        threadpool_free(tp);
+}
--- threadpool.h
+++ threadpool.h
@@ -1,11 +1,13 @@
 #include <stdbool.h>
 
 typedef struct threadpool threadpool;
-typedef struct job_t job_t;
+typedef struct task task;
+typedef struct threadinfo threadinfo;
+void *worker(void* arg);
 
-void* worker_routine(void* arg);
-threadpool* threadpool_create(int n);
-void threadpool_submit(threadpool* tp, void (*function)(void*), void*);
-void threadpool_wait(threadpool* tp);
-void threadpool_waitAndDestroy(threadpool* tp);
-void threadpool_destroy(threadpool* tp, bool immediate_shutdown);
+threadpool *threadpool_create(int num_of_threads);
+void threadpool_submit(threadpool *tp, void (*task_function)(void*), void *task_arg);
+task threadpool_steal(threadinfo *thief_thread);
+void threadpool_free(threadpool *tp);
+void threadpool_wait(threadpool *tp);
+void threadpool_destroy(threadpool *tp);
